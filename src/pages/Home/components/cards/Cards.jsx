c0c2336dec8731db7d1caa9235e132f0efed2bfc
Matias Pereyra <matiasv@missionreadyhq.com>
2024-06-04 14:02:58 +1200

    add images

--- src/pages/Home/components/cards/Cards.jsx
+++ src/pages/Home/components/cards/Cards.jsx
@@ -4,21 +4,21 @@ const Cards = () =>{
     return(
         <section className={style.mainCardsContainer}>
             <div className={style.cardsContainer}>
-                <img className={style.imageContainer} src="src/assets/background.png"></img>
+                <img className={style.imageContainer} src="../background.png"></img>
                 <div className={style.textContainer}>
                     <h2 className={style.cardTittle}>Lorem Ipsum</h2>
                     <p className={style.cardText}>Eti veniam nisi</p>
                 </div>
             </div>
             <div className={style.cardsContainer}>
-                <img className={style.imageContainer} src="src/assets/background.png"></img>
+                <img className={style.imageContainer} src="../background.png"></img>
                 <div className={style.textContainer}>
                     <h2 className={style.cardTittle}>Lorem Ipsum</h2>
                     <p className={style.cardText}>Eti veniam nisi</p>
                 </div>
             </div>
             <div className={style.cardsContainer}>
-                <img className={style.imageContainer} src="src/assets/background.png"></img>
+                <img className={style.imageContainer} src="../background.png"></img>
                 <div className={style.textContainer}>
                     <h2 className={style.cardTittle}>Lorem Ipsum</h2>
                     <p className={style.cardText}>Eti veniam nisi</p>
